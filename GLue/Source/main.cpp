4be74fe9ec4dc969ca6336737ff06a115c7291c5
pedrogzb <pegonba10@gmail.com>
2024-03-08 20:54:24 +0100

    Creación de la ventana y configuración de GLFW
    Primero inicializa GLFW y se le indica la versión que se va a utilizar de OpenGL y el perfil que se va a utilizar. En este caso se va a utilizar la versión 3.3 y el perfil core para acceder a las capacidades modernas de OpenGL. Luego se intenta crear la ventana. Si se ha podido, se crea el contexto de OpenGL y se crear una función Callback para poder modificar el ancho de la pantalla.

---
+++ GLue/Source/main.cpp
@@ -0,0 +1,30 @@
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+void SetFramebufferSizeCallback(GLFWwindow* window, int width, int height);
+
+int main() {
+
+	const int HEIGHT = 800;
+	const int WIDTH  = 600;
+
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	
+	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "GLue funcionaa >_<", NULL, NULL);
+	if (!window) {
+		std::cout << "No se ha podido crear correctamente una ventana con GLFW" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window,SetFramebufferSizeCallback);
+	return 0;
+}
+
+void SetFramebufferSizeCallback(GLFWwindow* window, int width, int height) {
+	glViewport(0, 0, width, height);
+}
+
