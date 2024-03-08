3b0288f36d1e53a45774859b26607d92b66641ca
pedrogzb <pegonba10@gmail.com>
2024-03-08 23:26:54 +0100

    Creación de un Vertex Buffer
    Se crea un buffer de vértices para poder enviar bloques de datos de vértices directamente a la memoria de la GPU

--- GLue/Source/main.cpp
+++ GLue/Source/main.cpp
@@ -79,7 +79,17 @@ int main() {
 		glGetProgramInfoLog(programShader, 512, NULL, infoLog);
 		std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
 	}
+	/*Preparando los vertices y estructuras*/
+	float vertices[] = {
+		-0.5f, -0.5f, 0.0f,
+		 0.5f, -0.5f, 0.0f,
+		 0.5f,  0.5f, 0.0f
+	};
 
+	unsigned int VBO;
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 	
 
 	/*Loop en el que se realizan la operaciones de visualización*/
